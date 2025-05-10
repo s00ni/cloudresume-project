4b2b0ab507e94f5b7f7ec29e4916e8cfd0e5274b
Rachel <Rachel1@macbook-pro.mynetworksettings.com>
2025-05-09 22:10:07 -0400

    Fixed lambda function

--- backend/lambda_function.py
+++ backend/lambda_function.py
@@ -5,22 +5,32 @@ dynamodb = boto3.resource('dynamodb')
 def lambda_handler(event, context):
     #Get Table
     table=dynamodb.Table('cloudresume-visitor-counter')
-    response = table.get_item(
+    table.get_item(
         Key={
             'counter_id':1
         }
     )
 
     #Update Table
-    table.update_item(
-        Key={
-            'counter_id':1
-        },
+    response = table.update_item(
+        Key={'counter_id':1},
         UpdateExpression='SET visitor_count = visitor_count + :val',
-        ExpressionAttributeValues={
-            ':val':1
-        }
+        ExpressionAttributeValues={':val':1},
+        ReturnValues='UPDATED_NEW'
     )
+    #Set updated value as an integer for serialization prep
+    updated=int(response['Attributes']['visitor_count'])
 
-    item=response['Item']
-    return (item)
+    return {
+        #Required ke-value pairs for CORS
+        'statusCode': 200,
+        'headers': {
+            'Access-Control-Allow-Headers': 'Content-Type',
+            'Access-Control-Allow-Origin': '*',
+            'Access-Control-Allow-Methods': 'GET, POST, OPTIONS'
+        }
+        ,
+        #Set python dictionary as json string
+        'body': json.dumps({'visitor_count': updated})
+    }
+
