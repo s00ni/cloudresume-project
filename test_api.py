4c21aaf7c18cb129c0ae415268e0bca35243f446
Rachel <Rachel1@MacBook-Pro.local>
2025-05-16 16:52:11 -0400

    Finished test

--- test_api.py
+++ test_api.py
@@ -1,12 +1,32 @@
 import asyncio
-from playwright.sync_api import async_playwright, Playwright
+from playwright.async_api import async_playwright, Playwright
 
 async def run(playwright: Playwright):
+    #first call to the api
     context = await playwright.request.new_context()
-    response = await context.get("https://l7241ivbyh.execute-api.us-east-1.amazonaws.com/development/subpath")
-    assert response.ok
-    assert response.status == 200
-    json_body = await response.json()
-    assert isinstance(json_body["visitor_count"], int)
+    response1 = await context.post("https://l7241ivbyh.execute-api.us-east-1.amazonaws.com/development/subpath")
+    assert response1.ok
+    json_body1 = await response1.json()
+    firstCall = json_body1["visitor_count"]
+
+    #second call to the api
+    response2 = await context.post("https://l7241ivbyh.execute-api.us-east-1.amazonaws.com/development/subpath")
+    assert response2.ok
+    json_body2 = await response2.json()
+    secondCall = json_body2["visitor_count"]
+
+    #check that the second call is incremented from first call by 1
+    assert secondCall == firstCall + 1
+
+    #check that a malformed payload returns a 500 error
+    badResponse = await context.get("https://l7241ivbyh.execute-api.us-east-1.amazonaws.com/development/subpath")
+    badResponse.status == 403
+
+
+async def main():
+    async with async_playwright() as playwright:
+        await run(playwright)
+
+asyncio.run(main())
 
 
